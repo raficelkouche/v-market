cf1e97458aadd9d6b115760ac36b4d0ed7d3b353
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2021-02-19 05:04:31 +0000

    refactor game.js code, move store related items to store.js

--- assets/js/store.js
+++ assets/js/store.js
@@ -32,6 +32,7 @@ class Store extends Phaser.Scene {
     this.load.image('tile', 'maps/vMarketTilesCROPPED.png');
     //load store html
     this.load.html('store_window', 'templates/store_window.html');
+    this.key = this.input.keyboard.addKeys("ESC")
   }
 
   create() {
@@ -306,152 +307,149 @@ class Store extends Phaser.Scene {
           addToCart(result)
         })
         // console.log(result)
-      }
-      )
+      })
     })
 
       
       // view cart
-      $("#checkout").off().on("click", () => { //need to replace
-        // cart info available - show page
-        // console.log(cart)
-        const total = cartTotal(cart)
-        // checkout page set up
-        $('#checkout').css("visibility", "hidden");
-        $('#products-grid').remove();
-        $('#product-container').remove();
-        // if cart has items
-        if (cart.length > 0) {
-          $('#products').append(`
-            <div id="checkout-table">
-              <h1>Review Order</h1>
-              <table class="table table-bordered">
-                <thead class="table-dark">
-                  <tr id="line-item-row">
-                    <td style="width: 50px; height:50px;"></td>
-                    <td>Name</td>
-                    <td>Description</td>
-                    <td>Price</td>
-                    <td>Remove</td>
-                  </tr>
-                </thead>
-                <tbody></tbody>
-              </table>
-              <div id="proceed">
-                <button id='back-button' class='btn btn-outline-warning'><i class="fas fa-chevron-circle-left"></i> Back </button>
-                <button id="checkout-button" class="btn btn-primary"><i class="far fa-credit-card"></i> Proceed</button>
-              </div>
-            </div>
-          
-            `)
-          $('tbody').append(checkOutList(cart))
-          $('tbody').append(`<tr id="line-item-row"><td colspan="3" id="order-total">Order Total</td><td style="width: 20%">$${total}</td></tr>`)
-          // checkout button function
-          $('#checkout-button').on('click', () => {
-            console.log('checkout button hit!')
-            console.log(this.playerInfo.id)
-            const data = {
-              user_id: this.playerInfo.id,
-              store_id: this.storeId,
-              total_price: total,
-              cart: cart
-            }
-            // COMPLETE ORDER AND RENDER CONFIMRATION PAGE
-            $.ajax(`/users/${this.playerInfo.id}/orders`, {method: 'POST', data: { data: data}})
-            .then((order) => {
-              if(order) { // sucessful and returns the order
-                console.log('ajax for checkout called')
-                console.log(order)
-                //  FINALLY !!!! order received back as obj
-                const orderItems = cart;
-                // empty cart
-                cart = [];
-                $('#checkout-cart-count').html(cart.length)
-                // rerender with order details
-                $('#checkout-table').remove()
-                $('#products').append(`
-                  <div id="checkout-table">
-                    <h1>Order Confirmation</h1>
-                    <p style="font-size: medium"> Hello ${this.playerInfo.name}. Thank you for your purchase!<p>
-                    <p style="font-size: medium"> Your Order Number is <b> ${order.order.id}.</b>
-                    <br>
-                      <table class="table table-bordered">
-                        <thead class="table-dark">
-                          <tr id="line-item-row">
-                            <td style="width: 50px; height:50px;"></td>
-                            <td>Name</td>
-                            <td>Description</td>
-                            <td>Price</td>
-                          </tr>
-                        </thead>
-                        <tbody></tbody>
-                      </table>
-                    <div id="proceed">
-                      <button id='back-button' class='btn btn-outline-warning'><i class="fas fa-chevron-circle-left"></i> Back </button>
-                      <button id='exit-button' class='btn btn-outline-danger'> Exit </button>
-                    </div>
-                  </div>
-                `)
-                $('tbody').append(orderList(orderItems))
-                $('tbody').append(`<tr id="line-item-row"><td colspan="3" id="order-total">Order Total</td><td style="width: 20%">$${total}</td></tr>`)
-
-                // add exit function for order confirmation page
-                $("#exit-button").on("click", () => {
-                  this.scene.start('Game', this.playerInfo);
-                })
-                
-                //  CART GETS MESSED UP WITH GOING BACK TO SHOP lol
-                // // add back function for order confimration page
-                $("#back-button").on("click", () => {
-                  // turn the checkout button on
-                  back(true);
-                  // to load more products
-                  $("#request-data").on("click", () => { //wait for helper
-                      requestItemData(storeID);
-                  })
-                })
-
-              } else {
-                res.json('Oops! something went wrong?')
-              }
-                })
-            })
-          // add remove function
-          for (let product of cart) {
-            $(`#remove-cart${cart.indexOf(product)}`).on("click", () => {
-              removeFromCart(cart.indexOf(product))
-              // rerender the page with update
-              $('#back-button').click()
-              $("#checkout").click() //need to replace
-            })
-          }
-        } else {
-          $('#products').append(`
-            <div id="checkout-table">
-              <h1>Review Order</h1>
-              <p>Hello! You have no items in the cart. Go back to the store page to view the our products. </p>
+    $("#checkout").off().on("click", () => { //need to replace
+      // cart info available - show page
+      // console.log(cart)
+      const total = cartTotal(cart)
+      // checkout page set up
+      $('#checkout').css("visibility", "hidden");
+      $('#products-grid').remove();
+      $('#product-container').remove();
+      // if cart has items
+      if (cart.length > 0) {
+        $('#products').append(`
+          <div id="checkout-table">
+            <h1>Review Order</h1>
+            <table class="table table-bordered">
+              <thead class="table-dark">
+                <tr id="line-item-row">
+                  <td style="width: 50px; height:50px;"></td>
+                  <td>Name</td>
+                  <td>Description</td>
+                  <td>Price</td>
+                  <td>Remove</td>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+            <div id="proceed">
               <button id='back-button' class='btn btn-outline-warning'><i class="fas fa-chevron-circle-left"></i> Back </button>
+              <button id="checkout-button" class="btn btn-primary"><i class="far fa-credit-card"></i> Proceed</button>
             </div>
-          `)
-        }
+          </div>
         
-        // return button to take back to store front
-        $("#back-button").on("click", () => {
-          back(true);
-          // to load more products
-          $("#request-data").on("click", () => { //wait for helper
-            requestItemData(storeID);
+          `)
+        $('tbody').append(checkOutList(cart))
+        $('tbody').append(`<tr id="line-item-row"><td colspan="3" id="order-total">Order Total</td><td style="width: 20%">$${total}</td></tr>`)
+        // checkout button function
+        $('#checkout-button').on('click', () => {
+          console.log('checkout button hit!')
+          console.log(this.playerInfo.id)
+          const data = {
+            user_id: this.playerInfo.id,
+            store_id: this.storeId,
+            total_price: total,
+            cart: cart
+          }
+          // COMPLETE ORDER AND RENDER CONFIMRATION PAGE
+          $.ajax(`/users/${this.playerInfo.id}/orders`, {method: 'POST', data: { data: data}})
+          .then((order) => {
+            if(order) { // sucessful and returns the order
+              console.log('ajax for checkout called')
+              console.log(order)
+              //  FINALLY !!!! order received back as obj
+              const orderItems = cart;
+              // empty cart
+              cart = [];
+              $('#checkout-cart-count').html(cart.length)
+              // rerender with order details
+              $('#checkout-table').remove()
+              $('#products').append(`
+                <div id="checkout-table">
+                  <h1>Order Confirmation</h1>
+                  <p style="font-size: medium"> Hello ${this.playerInfo.name}. Thank you for your purchase!<p>
+                  <p style="font-size: medium"> Your Order Number is <b> ${order.order.id}.</b>
+                  <br>
+                    <table class="table table-bordered">
+                      <thead class="table-dark">
+                        <tr id="line-item-row">
+                          <td style="width: 50px; height:50px;"></td>
+                          <td>Name</td>
+                          <td>Description</td>
+                          <td>Price</td>
+                        </tr>
+                      </thead>
+                      <tbody></tbody>
+                    </table>
+                  <div id="proceed">
+                    <button id='back-button' class='btn btn-outline-warning'><i class="fas fa-chevron-circle-left"></i> Back </button>
+                    <button id='exit-button' class='btn btn-outline-danger'> Exit </button>
+                  </div>
+                </div>
+              `)
+              $('tbody').append(orderList(orderItems))
+              $('tbody').append(`<tr id="line-item-row"><td colspan="3" id="order-total">Order Total</td><td style="width: 20%">$${total}</td></tr>`)
+
+              // add exit function for order confirmation page
+              $("#exit-button").on("click", () => {
+                this.scene.start('Game', this.playerInfo);
+              })
+              
+              //  CART GETS MESSED UP WITH GOING BACK TO SHOP lol
+              // // add back function for order confimration page
+              $("#back-button").on("click", () => {
+                // turn the checkout button on
+                back(true);
+                // to load more products
+                $("#request-data").on("click", () => { //wait for helper
+                    requestItemData(storeID);
+                })
+              })
+
+            } else {
+              res.json('Oops! something went wrong?')
+            }
           })
         })
+          // add remove function
+        for (let product of cart) {
+          $(`#remove-cart${cart.indexOf(product)}`).on("click", () => {
+            removeFromCart(cart.indexOf(product))
+            // rerender the page with update
+            $('#back-button').click()
+            $("#checkout").click() //need to replace
+          })
+        }
+      } else {
+        $('#products').append(`
+          <div id="checkout-table">
+            <h1>Review Order</h1>
+            <p>Hello! You have no items in the cart. Go back to the store page to view the our products. </p>
+            <button id='back-button' class='btn btn-outline-warning'><i class="fas fa-chevron-circle-left"></i> Back </button>
+          </div>
+        `)
+      }
+      
+      // return button to take back to store front
+      $("#back-button").on("click", () => {
+        back(true);
+        // to load more products
+        $("#request-data").on("click", () => { //wait for helper
+          requestItemData(storeID);
+        })
       })
-      // viewing single product
-    
+    })
   }
-
+    
   update() {
-
-
-
+    if (this.key.ESC.isDown) {
+      this.scene.start('Game', this.playerInfo);
+    }
   }
 }
 
